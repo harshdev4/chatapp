ccce6f76aff020e4f832a18e8de442a45b22d0b2
HarshWebd <upsign443@gmail.com>
2025-08-14 21:42:11 +0530

    changed the ui of setting page

--- Client/src/components/SettingPage.jsx
+++ Client/src/components/SettingPage.jsx
@@ -65,7 +65,7 @@ const SettingPage = () => {
   return (<>
     {isUser && userData.username && userData.name && <div className='w-full h-[100vh] flex justify-center items-center sm:justify-normal'>
         {isLoading && <div className="absolute z-10 top-[50%] left-[50%] translate-[-50%] w-[70px] h-[70px] border-b-3 border-l-3 rounded-[50%] animate-spin"> </div> }
-        <div className='grid grid-rows-[1fr_max-content_1fr_1fr_1fr] gap-[25px] w-[90%] h-[70vh] m-auto sm:w-[500px] rounded-[12px] shadow-[0_0_7px_black] p-4 bg-white'>
+        <div className='grid grid-rows-[1fr_max-content_1fr_1fr_1fr] gap-[25px] w-[90%] h-[100svh] m-auto sm:w-[500px] rounded-[12px] shadow-[0_0_7px_black] p-4 bg-white'>
             <div className='flex justify-center items-center gap-2.5'>
                 <IoMdSettings size={30}/> 
                 <h1 className='text-center text-4xl font-semibold'>Setting</h1>
@@ -75,7 +75,7 @@ const SettingPage = () => {
             <div className="imageSection w-full flex flex-col gap-2.5 items-center">
                 <div className='relative w-32 h-32 rounded-full border-2 border-[#949191] shadow-[0_0_3px_black_inset] overflow-hidden'>
                     <input ref={fileRef} type="file" name="profilePic" className='hidden' onChange={handleSelectFile}/>
-                    <img src={profileImage ? profileImage : `${userData.profilePic.slice(0, 50)}w_150,h_150/${userData.profilePic.slice(50,)}`} alt="profile" className='rounded-full w-full h-full object-cover'/>
+                    <img src={profileImage ? profileImage : `${userData.profilePic.slice(0, 50)}w_150/${userData.profilePic.slice(50,)}`} alt="profile" className='rounded-full w-full h-full object-cover'/>
                     <FaCamera className='absolute bottom-4 right-5 shadow-md text-[#b4b2b2] cursor-pointer' size={20} onClick={()=>fileRef.current.click()}/>
                 </div>
                 <p className='text-[0.8rem] text-[#73767b] text-center'>Click the camera icon to change the profile</p>
